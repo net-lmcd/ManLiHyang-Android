6e83fe12fba5b04e3f0993628823c6a3f398c08f
C.H Choi <satls34@naver.com>
2019-10-27 22:36:03 +0900

    ManLiHyang Application

---
+++ app/src/main/java/com/example/sideproject/side/contents/view/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.example.sideproject.side.contents.view.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sideproject.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/example/sideproject/side/contents/view/main/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.sideproject.side.contents.view.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sideproject.R
+import io.grpc.ManagedChannelBuilder
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
